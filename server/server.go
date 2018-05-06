a262d59588cf1cd1411f9d71664c83318a2e6e0c
xiaohuokevin <735564509@qq.com>
2018-05-06 22:58:51 +0800

    优化server端方法

--- server/server.go
+++ server/server.go
@@ -8,6 +8,8 @@ import (
 	"os"
 )
 
+const FILE_DIR  = "/data/static/img"
+
 //检查目录是否存在
 func checkFileIsExist(filename string) bool {
 	var exist = true
@@ -32,8 +34,8 @@ func main() {
 
 // 处理/upload 逻辑
 func upload(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("method:", r.Method) //获取请求的方法
-	fmt.Println(r)
+	//fmt.Println("method:", r.Method) //获取请求的方法
+	//fmt.Println(r)
 	if r.Method == "POST" {
 		r.ParseMultipartForm(32 << 20)
 		file, handler, err := r.FormFile("upfile")
@@ -42,32 +44,29 @@ func upload(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 		defer file.Close()
-		fmt.Fprintf(w, "%v", handler.Header)
+		//fmt.Fprintf(w, "%v", handler.Header)
 
 		//
-		bool_fileexist := checkFileIsExist("./test")
-		fmt.Println("check file 1-----------------")
-		fmt.Println("-------------------------bool_fileexist:", bool_fileexist)
+		bool_fileexist := checkFileIsExist(FILE_DIR)
+		//fmt.Println("check file 1-----------------")
+		//fmt.Println("-------------------------bool_fileexist:", bool_fileexist)
 		if bool_fileexist { //如果文件夹存在
 			//f, err1 = os.OpenFile(filename, os.O_APPEND, 0666) //打开文件
-			f, err := os.OpenFile("./test/"+handler.Filename, os.O_WRONLY|os.O_CREATE, 0666)
+			f, err := os.OpenFile(FILE_DIR+handler.Filename, os.O_WRONLY|os.O_CREATE, 0666)
 			if err != nil {
 				fmt.Println(err)
 				return
 			}
 			defer f.Close()
 			io.Copy(f, file)
-			fmt.Println("文件夹存在")
 		} else { //不存在文件夹时 先创建文件夹再上传
-			err1 := os.Mkdir("./test", os.ModePerm) //创建文件夹
+			err1 := os.Mkdir(FILE_DIR, os.ModePerm) //创建文件夹
 			if err1 != nil {
 				fmt.Println(err)
 				return
 			}
 
-			fmt.Println("文件夹不存在")
-			fmt.Println("文件夹创建成功！")
-			f, err := os.OpenFile("./test/"+handler.Filename, os.O_WRONLY|os.O_CREATE, 0666)
+			f, err := os.OpenFile(FILE_DIR+handler.Filename, os.O_WRONLY|os.O_CREATE, 0666)
 			if err != nil {
 				fmt.Println(err)
 				return
@@ -75,6 +74,5 @@ func upload(w http.ResponseWriter, r *http.Request) {
 			defer f.Close()
 			io.Copy(f, file)
 		}
-
 	}
 }
