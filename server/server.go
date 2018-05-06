4bd805b7039797f96ac083934953a3e2b2c50d8e
xiaohuokevin <735564509@qq.com>
2018-05-06 23:50:01 +0800

    server优化

--- server/server.go
+++ server/server.go
@@ -6,9 +6,13 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"encoding/json"
+	"strings"
 )
 
-const FILE_DIR  = "/data/static/img"
+const FILE_DIR  = "/data/static/img/"
+
+const ALI_STATIC_URL  = "http://static.xiaohuo.me/img/"
 
 //检查目录是否存在
 func checkFileIsExist(filename string) bool {
@@ -21,7 +25,6 @@ func checkFileIsExist(filename string) bool {
 }
 
 func main() {
-
 	http.HandleFunc("/xupload", upload)
 
 	err := http.ListenAndServe(":9090", nil) //设置监听的端口
@@ -34,6 +37,12 @@ func main() {
 
 // 处理/upload 逻辑
 func upload(w http.ResponseWriter, r *http.Request) {
+
+	type Ret struct {
+		Status	string
+		Message	string
+	}
+
 	//fmt.Println("method:", r.Method) //获取请求的方法
 	//fmt.Println(r)
 	if r.Method == "POST" {
@@ -44,14 +53,9 @@ func upload(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 		defer file.Close()
-		//fmt.Fprintf(w, "%v", handler.Header)
 
-		//
 		bool_fileexist := checkFileIsExist(FILE_DIR)
-		//fmt.Println("check file 1-----------------")
-		//fmt.Println("-------------------------bool_fileexist:", bool_fileexist)
 		if bool_fileexist { //如果文件夹存在
-			//f, err1 = os.OpenFile(filename, os.O_APPEND, 0666) //打开文件
 			f, err := os.OpenFile(FILE_DIR+handler.Filename, os.O_WRONLY|os.O_CREATE, 0666)
 			if err != nil {
 				fmt.Println(err)
@@ -74,5 +78,14 @@ func upload(w http.ResponseWriter, r *http.Request) {
 			defer f.Close()
 			io.Copy(f, file)
 		}
+
+		ret := Ret{"200", ALI_STATIC_URL + handler.Filename}
+		result, err := json.Marshal(ret)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		fmt.Fprintln(w,strings.ToLower(string(result)))
 	}
 }
